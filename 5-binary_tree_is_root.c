1843728a89ae99f8fce42233abce0e0a714f55aa
fredlaryea2000 <fredlaryea2000@gmail.com>
2023-08-30 15:19:09 -0700

    Updated

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - Verifies whether a given node serves as the root of a binary tree.
+ * binary_tree_is_root - Verifies whether a given node
+ * serves as the root of a binary tree.
  * @node: A reference to the node being assessed.
  *
  * Return: If the node is a root - 1.
